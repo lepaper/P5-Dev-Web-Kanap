fdb4ccb7ad7c4d2351eac59c68ef478d652fdeb0
Dominique De Doncker <dominiquededoncker0@gmail.com>
2022-05-26 12:04:16 +0200

    initial

---
+++ front/js/product.js
@@ -0,0 +1,20 @@
+const product = document.getElementById('item');
+const productcontenair = document.querySelector('article');
+
+// Lien entre page accueil et produit
+
+let params = new URL(document.location).searchParams;
+let canapId = params.get'id';
+
+
+let canapData = [];
+
+// Chercher les info de l'API
+const getproductbyID = async () => {
+    try {
+
+        const res = await fetch(`http://localhost:3000/api/products/${canapId}`)
+        canapData = await res.json
+
+        console.log(canapData);
+    }
