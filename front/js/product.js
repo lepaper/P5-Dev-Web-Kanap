b9462ea339c2d75a6df2d037ddb72f046d723101
Dominique De Doncker <dominiquededoncker0@gmail.com>
2022-05-26 15:44:52 +0200

    modif

--- front/js/product.js
+++ front/js/product.js
@@ -21,4 +21,46 @@ const getproductbyID = async () => {
     catch {
         alert('Problème avec fetch')
     }
-}   
+} 
+getproductbyID();
+
+//  Afficher les produit en fonction de l'ID
+
+const DisplayById = async () => {
+
+    await getproductbyID();
+
+    let productImg = document.querySelector('.item__img');
+    productImg.innerHTML = `<img src="${canapData.imageUrl}" alt="${canapData.altTxt}">`;
+
+    let productTitle = document.getElementById('title');
+    productTitle.innerText = canapData.name;
+
+    let productPrice = document.getElementById('price');
+    productPrice.innerText = canapData.price;
+
+    let productDescription = document.getElementById('description');
+    productDescription.innerHTML = canapData.description;
+
+    // Mapping pour choix de la couleur (plusieurs éléments)
+    document.querySelector("#colors").insertAdjacentHTML
+        ("beforeend", canapData.colors.map(colors => `<option value="${colors}">${colors}</option>`)
+            .join());
+
+}
+
+DisplayById()
+
+
+document.querySelector('#addToCart').addEventListener('click', function () {
+
+    // Récupération des options du produits : 
+
+    let optionProduct = {
+        name: canapData.name,
+        id: canapId,
+        color: document.getElementById('colors').value,
+        quantity: Number(document.getElementById('quantity').value),
+
+    }
+})
