46b3636e7a8de44d27c6df8f9bc7479ea403f28c
Dominique De Doncker <dominiquededoncker0@gmail.com>
2022-05-27 19:22:10 +0200

    modif

--- front/js/cart.js
+++ front/js/cart.js
@@ -36,5 +36,93 @@ const getCanapData = async () => {
         // Calcul prix
 
     }
-
 }
+getCanapData()
+
+
+// Afficher le tableau récapitulatif des achats 
+
+const displayBasket = (id, realId) => {
+
+
+    let cartArticle = document.createElement("article");
+    document.getElementById('cart__items').appendChild(cartArticle);
+    cartArticle.className = "cart__item";
+    cartArticle.setAttribute('data-id', id.id);
+    cartArticle.setAttribute('data-color', id.color);
+
+
+    // IMG
+
+    let divCartImg = document.createElement("div");
+    cartArticle.appendChild(divCartImg);
+    divCartImg.className = "cart__item__content";
+    let cartImg = document.createElement("img");
+    divCartImg.appendChild(cartImg);
+    cartImg.src = realId.imageUrl;
+    cartImg.alt = realId.altTxt;
+
+    // Description Bloc 
+
+    let divItemContent = document.createElement("div");
+    divItemContent.className = 'cart__item__content';
+    cartArticle.appendChild(divItemContent);
+    let divItemDescription = document.createElement('div');
+    divItemContent.appendChild(divItemDescription);
+    divItemDescription.className = 'cart__item__content__description';
+
+    // Description content : Title
+
+    let productName = document.createElement("h2");
+    productName.textContent = id.name;
+    divItemDescription.appendChild(productName);
+
+    // Description content : color
+
+    let productColor = document.createElement('p');
+    productColor.textContent = id.color;
+    divItemDescription.appendChild(productColor);
+
+    // Description content : price 
+
+    let productPrice = document.createElement('p');
+    productPrice.textContent = realId.price + ' ' + '€';
+    divItemDescription.appendChild(productPrice);
+
+
+    // Settings bloc 
+
+    let settingsBloc = document.createElement('div');
+    settingsBloc.className = 'cart__item__content__settings';
+    divItemContent.appendChild(settingsBloc);
+    let settingsQuantity = document.createElement('div');
+    settingsQuantity.className = 'cart__item__content__settings__quantity';
+    settingsBloc.appendChild(settingsQuantity);
+
+    // Settings bloc : manage quantity (add)
+
+    let quantityTxt = document.createElement('p');
+    quantityTxt.textContent = 'Qte : ';
+    settingsQuantity.appendChild(quantityTxt);
+    let inputQuantity = document.createElement('input');
+    inputQuantity.setAttribute("type", "number");
+    inputQuantity.setAttribute("name", "itemQuantity");
+    inputQuantity.setAttribute("min", 1);
+    inputQuantity.setAttribute("max", 100);
+    inputQuantity.setAttribute("value", id.quantity);
+    inputQuantity.classname = 'itemQuantity';
+    settingsQuantity.appendChild(inputQuantity);
+    inputQuantity.addEventListener('change', function (q) {
+        id.quantity = inputQuantity.value;
+        if (id.quantity <= 0 || id.quantity > 100) {
+            return alert('Veuillez choisir une quantité comprise entre 1 et 100')
+        }
+        {
+            localStorage.setItem("product", JSON.stringify(itemInCart));
+            location.reload();
+
+            console.log(itemInCart);
+        }
+
+
+    })
