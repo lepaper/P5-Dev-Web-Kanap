e7031eca131267e3bce8bdb94763fbeea7b580e3
Dominique De Doncker <dominiquededoncker0@gmail.com>
2022-05-31 08:08:20 +0200

    modif

---
+++ front/js/confirmation.js
@@ -0,0 +1,10 @@
+let orderId = new URL(window.location.href).searchParams;
+let id = orderId.get('orderId');
+
+function getOrderId() {
+    const displayOrderId = document.getElementById("orderId");
+    displayOrderId.innerText = id;
+    localStorage.clear();
+}
+
+getOrderId()
