6f8e12ebe668896ba6c5b4be3550a1a5c593749d
Dominique De Doncker <dominiquededoncker0@gmail.com>
2022-06-01 09:00:58 +0200

    modif

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,10 +1,13 @@
-let orderId = new URL(window.location.href).searchParams;
-let id = orderId.get('orderId');
 
-function getOrderId() {
-    const displayOrderId = document.getElementById("orderId");
-    displayOrderId.innerText = id;
-    localStorage.clear();
-}
+// recuperation du lien et de l'orderId
+let queryString = window.location.search;
+let urlParams = new URLSearchParams(queryString);
+let orderId = urlParams.get('orderId');
 
-getOrderId()
+//insertion de l'orderId dans l'HTML
+let orderNumber = document.querySelector('#orderId');
+orderNumber.innerHTML = orderId + ' <br> Merci de votre commande !';
+
+//suppression du localStorage
+let removeStorage = window.localStorage;
+removeStorage.clear();
