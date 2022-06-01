8135ae0681f3c87322b22e080e657a2f9252fe2b
Dominique De Doncker <dominiquededoncker0@gmail.com>
2022-06-01 11:21:15 +0200

    modif

--- front/js/script.js
+++ front/js/script.js
@@ -39,26 +39,3 @@ getallproducts()
     })
     .catch()
 
-// Afficher les canapés sur la page d'accueil et lien page produit 
-/* 
-const allproductsdisplay = async () => {
-
-    await getallproducts();
-
-    allproducts.innerHTML = canapData.map((canap) =>
-
-    <a href="product.html?id=${canap._id}">
-        <article>
-            <img src="${canap.imageUrl}" alt="${canap.altTxt}"/>
-             <h3 class="productName">${canap.name}</h3>
-             <p class="productDescription">${canap.description}</p>
-             <p>${canap.price} <span id="price">
-              
-            </span>€</p>
-         </article>       
-    </a>
-
-)
-    .join("");
-}
-allproductsdisplay();*/
